75cf0facefc37f7e624347922eef0b655bfdc422
yui.saitou <spring.burst@icloud.com>
2022-03-09 12:36:58 +0900

    「EditServlet.javaを作成」

--- WebContent/WEB-INF/views/messages/index.jsp
+++ WebContent/WEB-INF/views/messages/index.jsp
@@ -4,12 +4,12 @@
     <c:param name="content">
     <h2>メッセージ一覧</h2>
     <ul>
-        <c:forEach var="message" items="${messages}">
+        <c:forEach var="message" items="${messages}"> <!-- messagesは全てのメッセージのこと。 -->
                         <li>
                     <a href="${pageContext.request.contextPath}/show?id=${message.id}">
                         <c:out value="${message.id}" />
                     </a>
-                    ：<c:out value="${message.title}"></c:out> &gt; <c:out value="${message.content}" />
+                    <c:out value="${message.title}"></c:out> &gt; <c:out value="${message.content}" />
                 </li>
             </c:forEach>
     </ul>
--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -14,17 +13,18 @@ import javax.servlet.http.HttpServletResponse;
 import models.Message;
 import utils.DBUtil;
 
+//show.jspによって呼び出される。
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class EditServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public EditServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,17 +35,21 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        //Message.javaのクエリ"getAllMessages"を実行し、結果をリストに格納する。
-        //getAllMessagesは全ての投稿を取得しホーム画面index.jsp表示させるために行う。
-        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+        //該当のIDのメッセージ1件をDBから取得。
+        Message m = em.find(Message.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        //リクエストスコープに値を設定し、index.jspに渡す。
-        request.setAttribute("messages", messages);
+        // メッセージ情報とセッションIDをリクエストスコープに登録
+        request.setAttribute("message", m);  //メッセージ情報
+        request.setAttribute("_token", request.getSession().getId());  //セッションID
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        // メッセージIDをセッションスコープに登録
+        request.setAttribute("message_id", m.getId());
+
+        //edit.jspに値を渡して遷移する。
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
-
     }
+
 }
